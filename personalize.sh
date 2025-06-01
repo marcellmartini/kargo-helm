457e88fbfde8f97b85bfdb335e99908c79424707
Marcell Martini <1556478+marcellmartini@users.noreply.github.com>
2025-06-01 13:09:41 -0400

    personalize manifests

--- personalize.sh
+++ personalize.sh
@@ -9,8 +9,8 @@ username=$1
 repository_name=$2
 image_name=$3
 
-find . -type f -name '*.yaml' -exec sed -E -i.bak s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
-find . -type f -name '*.yaml' -exec sed -E -i.bak s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g {} +
+find . -type f -name '*.yaml' -exec sed -E -i.bak s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/"${username}"#g {} +
+find . -type f -name '*.yaml' -exec sed -E -i.bak s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/"${username}"#g {} +
 
 if [ ! -z "$repository_name" ]; then
   find . -type f -name '*.yaml' -exec sed -E -i.bak "s#https://github.com/${username}/[-_a-zA-Z0-9]+#https://github.com/${username}/${repository_name}#g" {} +
